4299d6901ab6500aadd429504991870926f4857d
Champu <117635421+TheChampu@users.noreply.github.com>
2024-09-17 15:45:11 +0530

    Update z_repo.py

--- plugins/z_repo.py
+++ plugins/z_repo.py
@@ -5,7 +5,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 import config
 from ChampuMusic import app
-from config import LOGGERS as OWNERS
 from ChampuMusic.utils.database import add_served_chat, get_assistant
 
 
@@ -55,24 +54,6 @@ InlineKeyboardButton("ᴄʜᴀᴛʙᴏᴛ", url=f"https://github.com/TheChampu/C
     )
 
 
-@app.on_message(filters.command("clone"))
-async def clones(client: Client, message: Message):
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=f"""**ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ sᴜᴅᴏ ᴜsᴇʀ sᴏ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴄʟᴏɴᴇ ᴍᴇ.**\n**ᴄʟɪᴄᴋ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴀɴᴅ ʜᴏsᴛ ᴍᴀɴᴜᴀʟʟʏ ᴏᴛʜᴇʀᴡɪsᴇ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ ᴏʀ sᴜᴅᴏ ᴜsᴇʀs ғᴏʀ ᴄʟᴏɴᴇ.**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "sᴏᴜʀᴄᴇ", url=f"https://github.com/TheChampu/ChampuMusic"
-                    )
-                ]
-            ]
-        ),
-    )
-
-
-# --------------------------------------------------------------------------------- #
 
 
 @app.on_message(
@@ -86,50 +67,3 @@ async def bot_check(_, message):
     chat_id = message.chat.id
     await add_served_chat(chat_id)
 
-
-# --------------------------------------------------------------------------------- #
-
-
-import asyncio
-
-
-@app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
-async def add_allbot(client, message):
-    command_parts = message.text.split(" ")
-    if len(command_parts) != 2:
-        await message.reply(
-            "**⚠️ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @TheChampuBot`**"
-        )
-        return
-
-    bot_username = command_parts[1]
-    try:
-        userbot = await get_assistant(message.chat.id)
-        bot = await app.get_users(bot_username)
-        app_id = bot.id
-        done = 0
-        failed = 0
-        lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
-        await userbot.send_message(bot_username, f"/start")
-        async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1001423108989:
-                continue
-            try:
-
-                await userbot.add_chat_members(dialog.chat.id, app_id)
-                done += 1
-                await lol.edit(
-                    f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
-                )
-            except Exception as e:
-                failed += 1
-                await lol.edit(
-                    f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅɪɴɢ ʙʏ»** @{userbot.username}"
-                )
-            await asyncio.sleep(3)  # Adjust sleep time based on rate limits
-
-        await lol.edit(
-            f"**➻ {bot_username} ʙᴏᴛ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ🎉**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
-        )
-    except Exception as e:
-        await message.reply(f"Error: {str(e)}")
