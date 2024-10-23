ada5d7b50e236450fc07af132617ac7ad43b5419
Champu <117635421+TheChampu@users.noreply.github.com>
2024-10-23 15:38:01 +0530

    Update sticker.py

--- plugins/sticker.py
+++ plugins/sticker.py
@@ -150,7 +150,7 @@ async def get_document_from_file_id(
     )
 
 
-@app.on_message(filters.command("stickerid"))
+@app.on_message(filters.command("stickerid", "stid"))
 @capture_err
 async def sticker_id(_, message: Message):
     reply = message.reply_to_message
@@ -310,14 +310,79 @@ async def kang(client, message: Message):
     except StickerPngDimensions:
         await message.reply_text("The sticker png dimensions are invalid.")
 
+@app.on_message(filters.command("st"))
+def generate_sticker(client, message):
+    if len(message.command) == 2:
+        sticker_id = message.command[1]
+        try:
+            client.send_sticker(message.chat.id, sticker=sticker_id)
+        except Exception as e:
+            message.reply_text(f"Error: {e}")
+    else:
+        message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ sᴛɪᴄᴋᴇʀ ɪᴅ ᴀғᴛᴇʀ /st ᴄᴏᴍᴍᴀɴᴅ.")
+
+@app.on_message(filters.command("packkang"))
+async def _packkang(app :app,message):  
+    txt = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ....**")
+    if not message.reply_to_message:
+        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ')
+        return
+    if not message.reply_to_message.sticker:
+        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ sᴛɪᴄᴋᴇʀ')
+        return
+    if message.reply_to_message.sticker.is_animated or  message.reply_to_message.sticker.is_video:
+        return await txt.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ɴᴏɴ-ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ")
+    if len(message.command) < 2:
+        pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@{BOT_USERNAME}'
+    else :
+        pack_name = message.text.split(maxsplit=1)[1]
+    short_name = message.reply_to_message.sticker.set_name
+    stickers = await app.invoke(
+        pyrogram.raw.functions.messages.GetStickerSet(
+            stickerset=pyrogram.raw.types.InputStickerSetShortName(
+                short_name=short_name),
+            hash=0))
+    shits = stickers.documents
+    sticks = []
+    
+    for i in shits:
+        sex = pyrogram.raw.types.InputDocument(
+                id=i.id,
+                access_hash=i.access_hash,
+                file_reference=i.thumbs[0].bytes
+            )
+        
+        sticks.append(
+            pyrogram.raw.types.InputStickerSetItem(
+                document=sex,
+                emoji=i.attributes[1].alt
+            )
+        )
+
+    try:
+        short_name = f'stikcer_pack_{str(uuid4()).replace("-","")}_by_{app.me.username}'
+        user_id = await app.resolve_peer(message.from_user.id)
+        await app.invoke(
+            pyrogram.raw.functions.stickers.CreateStickerSet(
+                user_id=user_id,
+                title=pack_name,
+                short_name=short_name,
+                stickers=sticks,
+            )
+        )
+        await txt.edit(f"**ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴋᴀɴɢᴇᴅ ʟɪɴᴋ**!\n**ᴛᴏᴛᴀʟ sᴛɪᴄᴋᴇʀ **: {len(sticks)}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴘᴀᴄᴋ ʟɪɴᴋ",url=f"http://t.me/addstickers/{short_name}")]]))
+    except Exception as e:
+        await message.reply(str(e))
 
 __MODULE__ = "Sᴛɪᴄᴋᴇʀ"
 __HELP__ = """
 **COMMANDS:**
 
-• /stickerid - **ɢᴇᴛs ᴛʜᴇ ғɪʟᴇ ɪᴅ ᴏғ ᴀɴʏ ʀᴇᴘʟɪᴇᴅ sᴛɪᴄᴋᴇʀ.**
+• /stickerid , /stid - **ɢᴇᴛs ᴛʜᴇ ғɪʟᴇ ɪᴅ ᴏғ ᴀɴʏ ʀᴇᴘʟɪᴇᴅ sᴛɪᴄᴋᴇʀ.**
 • /getsticker - **ɢᴇᴛs ᴛʜᴇ ɪᴍᴀɢᴇ ᴏғ ᴀɴʏ ʀᴇᴘʟɪᴇᴅ sᴛɪᴄᴋᴇʀ.**
 • /kang - **ᴋᴀɴɢs ᴀɴʏ sᴛɪᴄᴋᴇʀ ɪɴ ᴛʜᴇ ʏᴏᴜ ᴘᴀᴄᴋ**
+• /st - **ᴄʜᴇᴄᴋ sᴛɪᴄᴋᴇʀ ғʀᴏᴍ sᴛɪᴄᴋᴇʀ ɪᴅ**
+• /packkang - **ᴋᴀɴɢ ᴡʜᴏʟᴇ sᴛɪᴄᴋᴇʀ ᴘᴀᴄᴋ**
 
 **INFO:**
 
