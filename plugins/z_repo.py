03f60220aa75ca50393ed2f11c4afd41c97c9d97
Champu <117635421+TheChampu@users.noreply.github.com>
2024-12-27 19:01:59 +0530

    Del

--- plugins/z_repo.py
+++ plugins/z_repo.py
@@ -55,15 +55,3 @@ InlineKeyboardButton("ᴄʜᴀᴛʙᴏᴛ", url=f"https://github.com/TheChampu/C
 
 
 
-
-@app.on_message(
-    filters.command(
-        ["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"],
-        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
-    )
-    & filters.group
-)
-async def bot_check(_, message):
-    chat_id = message.chat.id
-    await add_served_chat(chat_id)
-
