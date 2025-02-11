70da07030a1ffb19a74fe516e08837b01d8b7760
Champu <117635421+TheChampu@users.noreply.github.com>
2025-02-11 20:18:02 +0530

    Update truth.py

--- plugins/truth.py
+++ plugins/truth.py
@@ -1,44 +1,42 @@
 import requests
 from pyrogram import filters
-
 from ChampuMusic import app
 
 truth_api_url = "https://api.truthordarebot.xyz/v1/truth"
 dare_api_url = "https://api.truthordarebot.xyz/v1/dare"
 
-
 @app.on_message(filters.command("truth"))
-def get_truth(client, message):
+async def get_truth(client, message):
     try:
         response = requests.get(truth_api_url)
         if response.status_code == 200:
             truth_question = response.json()["question"]
-            message.reply_text(f"ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ:\n\n{truth_question}")
+            await message.reply_text(f"ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ:\n\n{truth_question}")
         else:
-            message.reply_text(
+            await message.reply_text(
                 "ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀ ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ."
             )
     except Exception as e:
-        message.reply_text(
+        await message.reply_text(
             "ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ғᴇᴛᴄʜɪɴɢ ᴀ ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ."
         )
 
-
 @app.on_message(filters.command("dare"))
-def get_dare(client, message):
+async def get_dare(client, message):
     try:
         response = requests.get(dare_api_url)
         if response.status_code == 200:
             dare_question = response.json()["question"]
-            message.reply_text(f"ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ:\n\n{dare_question}")
+            await message.reply_text(f"ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ:\n\n{dare_question}")
         else:
-            message.reply_text(
+            await message.reply_text(
                 "ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀ ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ."
             )
     except Exception as e:
-        message.reply_text(
+        await message.reply_text(
             "ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ғᴇᴛᴄʜɪɴɢ ᴀ ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ."
         )
+
 
 
 __HELP__ = """
